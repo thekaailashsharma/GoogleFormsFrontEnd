71cf45209493fe46483c00c45d7a801da3f7fe53
thekaailashsharma <kailashps.1011@gmail.com>
2024-06-20 00:41:11 +0530

    Create FrontEnd

---
+++ Google Forms App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Google_Forms_App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
